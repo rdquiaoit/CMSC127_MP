8b1771dd57af31eb3537965acb52dfd4a616fad6
rdquiaoit <62099374+rdquiaoit@users.noreply.github.com>
2020-08-11 15:13:25 +0800

    Add files via upload
    db code to access the database created

---
+++ db.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+                    host=host_name,
+                    user=user_name,
+                    passwd=user_password,
+                    database=db_name
+                    )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+
+
+
+def execute_query(query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+def execute_read_query(query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.commit()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+connection = create_connection("localhost", "root", "","covid19ph_db")
